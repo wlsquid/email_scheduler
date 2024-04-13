cee8975ca4673f1bc54ffe8b778f18f52d1857c6
wlsquid <wyndhammorland@gmail.com>
2024-04-13 14:59:44 +1000

    WIP repositories

---
+++ email_scheduler.Server/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace email_scheduler.Server.Interfaces
+{
+    public interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetEmails();
+
+        IEnumerable<T> GetAllByUserID(Guid userId);
+
+        T GetByID(Guid Id);
+
+        T Insert(T enitity);
+        void Delete(Guid Id);
+        T Update(T entity);
+        void Save();
+    }
+}
---
+++ email_scheduler.Server/Repositories/EmailRepository.cs
@@ -0,0 +1,15 @@
+﻿using email_scheduler.Server.Data;
+using email_scheduler.Server.Entities;
+using email_scheduler.Server.Interfaces;
+
+namespace email_scheduler.Server.Repositories
+{
+    //write test for this
+    public class EmailRepository : IRepository<Email>, IDisposable
+    {
+        private EmailSchedulerDbContext context;
+        public EmailRepository(EmailSchedulerDbContext context) { 
+            this.context = context;
+        }
+    }
+}
